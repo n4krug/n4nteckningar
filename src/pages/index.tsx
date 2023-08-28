2abf6f738dd00678bab51ae6db4a3946bf663069
Gustav Eneberg <gustav.eneberg@gmail.com>
2023-08-28 23:47:27 +0200

    update card metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,8 +30,8 @@ export default function Home(): JSX.Element {
 	const { siteConfig } = useDocusaurusContext();
 	return (
 		<Layout
-			title={`Hello from ${siteConfig.title}`}
-			description='Description will go into a meta tag in <head />'
+			title={`n4nteckningar`}
+			description='Mina anteckningar från föreläsningar'
 		>
 			<HomepageHeader />
 			<main>
